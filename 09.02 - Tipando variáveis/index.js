c86e47b3c7bef9ec1b197bcd1a739c96d13ca121
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-02-10 06:20:19 -0300

    09.02

---
+++ 09.02 - Tipando variáveis/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const personName = 'Daniel';
+const personAge = 22;
+const personAlive = true;
+const personContacts = [
+    0,
+    'daniel.bonifacio@algaworks.com',
+];
---
+++ 09.02 - Tipando variáveis/index.ts
@@ -0,0 +1,8 @@
+const personName:string = 'Daniel'
+const personAge:number = 22
+const personAlive:boolean = true
+
+const personContacts:(string|number)[] = [
+  0,
+  'daniel.bonifacio@algaworks.com',
+]
