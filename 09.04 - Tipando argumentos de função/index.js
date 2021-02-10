0273f97c1b9c9329b53064327707416496010529
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-02-10 07:05:45 -0300

    09.04

---
+++ 09.04 - Tipando argumentos de função/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet('Daniel');
+greet(0);
+greet(true);
---
+++ 09.04 - Tipando argumentos de função/index.ts
@@ -0,0 +1,7 @@
+function greet (name:string, age?:number) {
+  console.log(
+    `Hello, ${name.toUpperCase()}!`
+  )
+}
+
+greet('Daniel', 1)
