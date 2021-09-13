d21eabc07e1b38c92bf7ce79907bbaaed7078047
Daniel Bonifacio <contato@danielbonifacio.com.br>
2021-09-13 07:46:12 -0300

    Aula 09.10 - Generics

--- 09.10 - Generics/index.ts
+++ 09.10 - Generics/index.ts
@@ -1,27 +1,24 @@
 interface Photo {
-  albumId: number
-  id: number
-  title: string
-  url: string
-  thumbnailUrl: string
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
 
-fetch('http://jsonplaceholder.typicode.com/photos?_start=0&_limit=3')
-  .then(res => res.json())
-  .then(data => {
+fetch("http://jsonplaceholder.typicode.com/photos?_start=0&_limit=3");
 
-  })
-
-newFetch<Photo[]>('http://jsonplaceholder.typicode.com/photos?_start=0&_limit=3')
-  .then(photos => {
-    photos.data.map(photo => photo.id)
-  })
-
-async function newFetch <T> (url: string) {
-  const res = await fetch(url)
-  const data: T = await res.json()
+async function enhancedFetch <ResponseData = string[]> (url: string) {
+  const res = await fetch(url);
+  const data: ResponseData = await res.json();
+  
   return {
-    data: data,
-    status: res.status
-  }
+    status: res.status,
+    data
+  };
 }
+
+enhancedFetch<Photo[]>("http://jsonplaceholder.typicode.com/photos?_start=0&_limit=3")
+  .then(res => {
+    res.data.map(item => console.log(item.albumId))
+  });
